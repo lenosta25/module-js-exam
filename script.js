b3c2c371fce21fe1e8c08225b71d1d3030e65ec8
Lena <93406015+lenosta25@users.noreply.github.com>
2022-04-24 00:58:49 +0300

    Added to body title swiper content and meta

---
+++ script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper(".main__swiper", {
+  touchRatio: 2,
+  grabCursor: true,
+  slidesPerView:1,
+  spaceBetween: 30,
+  slideToClickedSlide: true,
+  keyboard: {
+    enabled: true,
+  },
+
+});
+
+
